fe6a59f5e23a199b18da18013d6e3485954fc165
EVTEV <august1.caes@gmail.com>
2025-02-14 08:41:27 +0100

    Update use_node.c

--- src/Node/use_node.c
+++ src/Node/use_node.c
@@ -65,6 +65,28 @@ void	rotate_to_top(t_stack *stack, t_node *target, char name)
 	}
 }
 
+void	smart_double_rotate(t_stack *a, t_stack *b, t_node *targ_a, t_node *targ_b)
+{
+	int	pos_a;
+	int	pos_b;
+
+	while (a->top != targ_a && b->top != targ_b)
+		{
+			pos_a = get_pos_node(a, targ_a);
+			pos_b = get_pos_node(b, targ_b);
+			if (pos_a <= a->size/2 && pos_b <= b->size/2)
+			{
+				rr(a, b)
+			}
+			else if (pos_a > a->size/2 && pos_b > b->sixe/2)
+			{
+				rrr(a, b);
+			}
+			else
+				break ;
+		}
+}
+
 void	push_node(t_stack *a, t_stack *b, int start, int end)
 {
 	t_node	*target;
@@ -73,7 +95,13 @@ void	push_node(t_stack *a, t_stack *b, int start, int end)
 	while (target)
 	{
 		rotate_to_top(a, target, 'a');
-		pb(b, a);
+		if (target->index < (start + end) / 2)
+		{
+			pb(b, a);
+			rb(b);
+		}
+		else
+			pb(b, a);
 		target = find_target_node(a, start, end);
 	}
 }
